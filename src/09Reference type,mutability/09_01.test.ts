f272f7e4c2bab8e675a6b0a05591bcc805bb63bf
Anton Charapanau <charapanauanton@gmail.com>
2023-03-22 14:37:18 +0000

    09

---
+++ src/09Reference type,mutability/09_01.test.ts
@@ -0,0 +1,15 @@
+function icreaseAge(u:UsersType) {
+    u.age++;
+}
+type UsersType = {
+    name:string
+    age:number
+}
+
+test('value type test',() => {
+   let usersCount = 100;
+
+   let adminsCount = usersCount;
+
+})
+
